294e4026c9f487120a94c12e24d23d965809e457
lukeisuh <luke@issilly.fr>
2023-09-25 19:17:15 +1000

    changin

--- vault.js
+++ vault.js
@@ -1,14 +1,21 @@
-// vault.js
 const handleImage = document.querySelector('.handle');
 const doorImage = document.querySelector('.door');
 const insideImage = document.querySelector('.inside');
 
 let isOpen = false;
+let isHandleTurning = false;
 
 handleImage.addEventListener('click', () => {
-    if (!isOpen) {
-        handleImage.style.transform = 'translateX(350px)';
-        doorImage.style.transform = 'translateX(350px)';
-        isOpen = true;
+    if (!isHandleTurning) {
+        handleImage.style.transform = 'rotate(360deg)';
+        isHandleTurning = true;
+
+        setTimeout(() => {
+            if (!isOpen) {
+                doorImage.style.transform = 'translateX(350px)';
+                handleImage.style.transform = 'translateX(350px)';
+                isOpen = true;
+            }
+        }, 1000);
     }
 });
